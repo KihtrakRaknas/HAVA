080fd82f2c1073baff3b50f60befd261bb795291
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2022-04-09 14:09:07 -0400

    remove unused code

--- packages/captive/src/index.js
+++ packages/captive/src/index.js
@@ -1,6 +1,5 @@
 import "./index.css";
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import {DAppProvider, Mainnet, Rinkeby} from "@usedapp/core";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -10,27 +9,18 @@ import App from "./App";
 // Change this to your own Infura project id: https://infura.io/register
 const INFURA_PROJECT_ID = "239ff2f143084d0f957c39a01c46998e";
 const config = {
-  readOnlyChainId: Rinkeby.chainId,
-  readOnlyUrls: {
-    [Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
-    [Rinkeby.chainId]: "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID,
-  },
+    readOnlyChainId: Rinkeby.chainId,
+    readOnlyUrls: {
+        [Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
+        [Rinkeby.chainId]: "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID,
+    },
 }
 
-// You should replace this url with your own and put it into a .env file
-// See all subgraphs: https://thegraph.com/explorer/
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
-});
-
 ReactDOM.render(
-  <React.StrictMode>
-    <DAppProvider config={config}>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </DAppProvider>
-  </React.StrictMode>,
-  document.getElementById("root"),
+    <React.StrictMode>
+        <DAppProvider config={config}>
+            <App/>
+        </DAppProvider>
+    </React.StrictMode>,
+    document.getElementById("root"),
 );
