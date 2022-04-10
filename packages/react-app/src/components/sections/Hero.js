312e58010f934f419e65f114908801b181cc5e94
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2022-04-10 10:03:55 -0400

    update video link

--- packages/react-app/src/components/sections/Hero.js
+++ packages/react-app/src/components/sections/Hero.js
@@ -79,7 +79,7 @@ const Hero = ({
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
-              data-video="https://player.vimeo.com/video/174002812"
+              data-video="https://www.youtube.com/embed/nw9KwqRjy-c"
               href="#0"
               aria-controls="video-modal"
               onClick={openModal}
@@ -96,7 +96,7 @@ const Hero = ({
             id="video-modal"
             show={videoModalActive}
             handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
+            video="https://www.youtube.com/embed/nw9KwqRjy-c"
             videoTag="iframe" />
         </div>
       </div>
